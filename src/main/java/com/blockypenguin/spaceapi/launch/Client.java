26d22082a3c5bcad175e36fbd37f1ee62b01084d
BlockyPenguin <me@blockypenguin.com>
2023-06-05 13:02:08 +0100

    Initial Commit

---
+++ src/main/java/com/blockypenguin/spaceapi/launch/Client.java
@@ -0,0 +1,11 @@
+package com.blockypenguin.spaceapi.launch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Client implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/blockypenguin/spaceapi/launch/Main.java
@@ -0,0 +1,12 @@
+package com.blockypenguin.spaceapi.launch;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class Main implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        
+    }
+}
